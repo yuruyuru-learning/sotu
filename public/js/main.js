68cc20f23d09ae655904687d5eb398b6dba1107f
yukiny0811 <yukiny0811@gmail.com>
2023-04-08 12:19:19 +0900

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  let ws = new WebSocket("ws://" + window.location.host + "/websocket")
+  let ws = new WebSocket("wss://" + window.location.host + "/websocket")
   ws.onmessage = msg => {
     $("#messageBox").prepend("<li>" + msg.data + "</li>")
   }
