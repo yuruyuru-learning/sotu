5fe9202c32ebb850a6abba5d1ab68101113fd253
yukiny0811 <yukiny0811@gmail.com>
2023-04-08 13:06:25 +0900

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 window.onload = () => {
   let ws = new WebSocket("wss://" + window.location.host + "/websocket")
   ws.onmessage = msg => {
-    $("#messageBox").prepend("<li>" + msg.data + "</li>")
+    $("#messageBox").append("<h3>" + msg.data + "</h3>")
   }
   $("#form").on("submit", e => {
     ws.send($("#send").val())
+    $("#send").val("")
     e.preventDefault()
   })
 }
