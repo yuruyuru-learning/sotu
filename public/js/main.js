ada7ca5c4438f2594cffb0b120d5f736286a8535
yukiny0811 <yukiny0811@gmail.com>
2023-04-08 14:49:24 +0900

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 let ws
 
 window.onload = () => {
-  ws = new WebSocket("wss://" + window.location.host + "/websocket")
+  ws = new ReconnectingWebSocket("wss://" + window.location.host + "/websocket")
   ws.onmessage = msg => {
     $("#messageBox").append("<div class='but'><h3>" + msg.data + "</h3></div>")
   }
   ws.onclose = () => {
-    ws = new WebSocket("wss://" + window.location.host + "/websocket")
+    ws = new ReconnectingWebSocket("wss://" + window.location.host + "/websocket")
     ws.send("0")
   }
 }
--- public/js/main2.js
+++ public/js/main2.js
@@ -1,13 +1,13 @@
 let ws
 
 window.onload = () => {
-    ws = new WebSocket("wss://" + window.location.host + "/websocket")
+    ws = new ReconnectingWebSocket("wss://" + window.location.host + "/websocket")
     $("#form").on("submit", e => {
       ws.send($("#send").val())
       e.preventDefault()
     })
     ws.onclose = () => {
-        ws = new WebSocket("wss://" + window.location.host + "/websocket")
+        ws = new ReconnectingWebSocket("wss://" + window.location.host + "/websocket")
         ws.send("0")
     }
 }
