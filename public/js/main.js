d65a9ee0d42b52df029871833e32d1f75930b151
yukiny0811 <yukiny0811@gmail.com>
2023-04-08 14:15:05 +0900

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,12 @@
 let ws
 
-function sayHi() {
-  ws.send("0")
-  ws.reconnect()
-  setTimeout(sayHi, 1000)
-}
-
 window.onload = () => {
   ws = new WebSocket("wss://" + window.location.host + "/websocket")
   ws.onmessage = msg => {
     $("#messageBox").append("<div class='but'><h3>" + msg.data + "</h3></div>")
   }
-
-  setTimeout(sayHi, 1000)
+  ws.onclose = () => {
+    ws = new WebSocket("wss://" + window.location.host + "/websocket")
+    ws.send("0")
+  }
 }
--- public/js/main2.js
+++ public/js/main2.js
@@ -1,17 +1,13 @@
 let ws
 
-function sayHi() {
-    ws.send("0")
-    ws.reconnect()
-    setTimeout(sayHi, 1000)
-}
-
 window.onload = () => {
     ws = new WebSocket("wss://" + window.location.host + "/websocket")
     $("#form").on("submit", e => {
       ws.send($("#send").val())
       e.preventDefault()
     })
-
-    setTimeout(sayHi, 1000)
+    ws.onclose = () => {
+        ws = new WebSocket("wss://" + window.location.host + "/websocket")
+        ws.send("0")
+    }
 }
