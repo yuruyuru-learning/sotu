52b31556ae286ef9f1910e27b6ed6ad1b9b32407
yukiny0811 <yukiny0811@gmail.com>
2023-04-08 11:52:10 +0900

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,11 @@
-window.addEventListener('load', () => {
-    let msgbox = document.getElementById('msgs');
-    let form = document.getElementById('form');
-    let sendMsg = document.getElementById('send-msg');
-    let ws = new WebSocket('wss://' + window.location.host + '/websocket');
-
-    ws.onopen = () => console.log('connection opened');
-    ws.onclose = () => console.log('connection closed');
-    ws.onmessage = m => {
-      let li = document.createElement('li');
-      li.textContent = m.data;
-      msgbox.insertBefore(li, msgbox.firstChild);
-    }
-
-    sendMsg.addEventListener('click', () => sendMsg.value = '');
-
-    form.addEventListener('submit', e => {
-      ws.send(sendMsg.value);
-      sendMsg.value = '';
-      e.preventDefault();
-    });
-  });
+window.onload = () => {
+  let ws = new WebSocket("ws://" + window.location.host + "/websocket")
+  ws.onmessage = msg => {
+    $("#messageBox").prepend("<li>" + msg.data + "</li>")
+  }
+  $("#form").on("submit", e => {
+    ws.send($("#send").val())
+    $("#send").val("")
+    e.preventDefault()
+  })
+}
